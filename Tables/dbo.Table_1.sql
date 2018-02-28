91c6880c1c71628a45a4b9ffd63d3920dd5952bd
Жданов Александр Леонидович <a.zhdanov@otlnal.ru>
2018-02-28 16:52:27 +0700

    add collumn first name

--- Tables/dbo.Table_1.sql
+++ Tables/dbo.Table_1.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[Table_1]
 (
 [name] [nchar] (10) COLLATE Cyrillic_General_CI_AS NULL,
-[last name] [nchar] (10) COLLATE Cyrillic_General_CI_AS NULL
+[last name] [nchar] (10) COLLATE Cyrillic_General_CI_AS NULL,
+[first_name] [nchar] (10) COLLATE Cyrillic_General_CI_AS NULL
 ) ON [PRIMARY]
 GO
